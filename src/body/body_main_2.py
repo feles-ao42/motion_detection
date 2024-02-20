a531c73cd04eabd4fdba0b37491ff2cba2ee48a0
feles-ao42 <yf@keio.jp>
2024-02-20 13:36:36 +0900

    fix

--- src/body/body_main_2.py
+++ src/body/body_main_2.py
@@ -79,7 +79,7 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-gesture_label = ['SAKURA', 'CAT', 'TRAIN']
+gesture_label = ['NORMAL', 'DOUZO', 'OTHER']
 
 while video_capture.isOpened():
     # カメラ画像取得
